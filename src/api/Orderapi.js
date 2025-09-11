3b45ea6a76371a2eb0c6c38f00c7685c47b3f134
Karthik7259 <mohankartik.cs23@rvce.edu.in>
2025-09-11 23:47:42 +0530

    updates api

--- src/api/Orderapi.js
+++ src/api/Orderapi.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://scarlit-backend.onrender.com/", // backend base URL
+  // baseURL: "http://localhost:5000/", // backend base URL
 });
 
 export const createOrder = (data) => API.post("/api/order/create", data);
